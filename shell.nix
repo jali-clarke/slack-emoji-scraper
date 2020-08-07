8c47721c4ded18755fd0b2b31c65d1c6ac20763a
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-08-07 19:55:22 +0000

    install requirements on shell load

--- shell.nix
+++ shell.nix
@@ -5,11 +5,13 @@ let nixpkgs = import <nixpkgs> {};
 
     python = nixpkgs."${pythonAttr}Full";
     pythonPackages = nixpkgs."${pythonAttr}Packages";
+    requirementsTxt = ./requirements.txt;
 in nixpkgs.mkShell {
     name = name;
     buildInputs = [
         python
         pythonPackages.pip
+        requirementsTxt
     ];
 
     shellHook = "
@@ -17,5 +19,6 @@ in nixpkgs.mkShell {
         export PIP_PREFIX=\"/tmp/${name}/pip_packages\"
         export PATH=\"$PIP_PREFIX/bin:$PATH\"
         export PYTHONPATH=\"$PIP_PREFIX/lib/python$(python --version | grep -o '[0-9]\\.[0-9]')/site-packages:$PYTHONPATH\"
+        python -m pip install -r ${requirementsTxt}
     ";
 }

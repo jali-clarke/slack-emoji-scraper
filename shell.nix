f77b9699defef0089c712862a8dacd4ff7550533
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-08-07 20:13:44 +0000

    add some more automation to shell.nix

--- shell.nix
+++ shell.nix
@@ -5,12 +5,14 @@ let nixpkgs = import <nixpkgs> {};
 
     python = nixpkgs."${pythonAttr}Full";
     pythonPackages = nixpkgs."${pythonAttr}Packages";
+    pip = pythonPackages.pip;
+
     requirementsTxt = ./requirements.txt;
 in nixpkgs.mkShell {
     name = name;
     buildInputs = [
         python
-        pythonPackages.pip
+        pip
         requirementsTxt
     ];
 
@@ -18,7 +20,7 @@ in nixpkgs.mkShell {
         unset SOURCE_DATE_EPOCH
         export PIP_PREFIX=\"/tmp/${name}/pip_packages\"
         export PATH=\"$PIP_PREFIX/bin:$PATH\"
-        export PYTHONPATH=\"$PIP_PREFIX/lib/python$(python --version | grep -o '[0-9]\\.[0-9]')/site-packages:$PYTHONPATH\"
-        python -m pip install -r ${requirementsTxt}
+        export PYTHONPATH=\"$PIP_PREFIX/lib/python$(${python}/bin/python --version | ${nixpkgs.gnugrep}/bin/grep -o '[0-9]\\.[0-9]')/site-packages:$PYTHONPATH\"
+        ${pip}/bin/pip install -r ${requirementsTxt}
     ";
 }

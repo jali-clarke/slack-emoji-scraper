609e899a666eecfd07041f619f0c64275ed65727
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-08-09 16:43:18 +0000

    update mkVenvShell version

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 let mkVenvShell = import(
         builtins.fetchGit {
             url = "https://github.com/jali-clarke/mkVenvShell.git";
-            ref = "refs/tags/0.0.0";
+            ref = "1.2.0";
         }
     );
 in mkVenvShell {
     projectName = "slack-emoji-scraper";
     python = "python38";
-    requirements = ./requirements.txt;
+    requirements = [./requirements.txt];
 }

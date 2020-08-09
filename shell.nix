b4a6f414859bba9ef5dc32496174fdde8db01b13
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-08-09 15:00:25 +0000

    use mkVenvShell helper

--- shell.nix
+++ shell.nix
@@ -1,26 +1,11 @@
-let nixpkgs = import <nixpkgs> {};
-
-    name = "slack-emoji-scraper";
-    pythonAttr = "python38";
-
-    python = nixpkgs."${pythonAttr}Full";
-    pythonPackages = nixpkgs."${pythonAttr}Packages";
-    pip = pythonPackages.pip;
-
-    requirementsTxt = ./requirements.txt;
-in nixpkgs.mkShell {
-    name = name;
-    buildInputs = [
-        python
-        pip
-        requirementsTxt
-    ];
-
-    shellHook = "
-        unset SOURCE_DATE_EPOCH
-        export PIP_PREFIX=\"/tmp/${name}/pip_packages\"
-        export PATH=\"$PIP_PREFIX/bin:$PATH\"
-        export PYTHONPATH=\"$PIP_PREFIX/lib/python$(${python}/bin/python --version | ${nixpkgs.gnugrep}/bin/grep -o '[0-9]\\.[0-9]')/site-packages:$PYTHONPATH\"
-        ${pip}/bin/pip install -r ${requirementsTxt}
-    ";
+let mkVenvShell = import(
+        builtins.fetchGit {
+            url = "https://github.com/jali-clarke/mkVenvShell.git";
+            ref = "refs/tags/0.0.0";
+        }
+    );
+in mkVenvShell {
+    projectName = "slack-emoji-scraper";
+    python = "python38";
+    requirements = ./requirements.txt;
 }
